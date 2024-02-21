8335d617dd569acd75de6ad1ba5118bb5f4db90f
Rhegis Marques <rhegismarques@gmail.com>
2024-02-21 18:01:06 -0300

    up

--- api.js
+++ api.js
@@ -1,30 +1,51 @@
-const input = document.querySelector('#input');
-const imgDigi = document.querySelector('#img-digi');
-const digiTitle = document.querySelector('.card-title');
-const digiText = document.querySelector('.card-text');
+const input = document.querySelector("#input");
+const imgDigi = document.querySelector("#img-digi");
+const digiTitle = document.querySelector(".card-title");
+const digiText = document.querySelector(".card-text");
 
 const fetchAPI = async () => {
-  const response = await fetch('https://digimon-api.vercel.app/api/digimon');
+  const url = 'https://digimon-api.vercel.app/api/digimon';
   const data = await response.json();
+
   return data;
 }
 
+async function digiCard({ target }) {
+  const digiArr = await fetchAPI();
+
+  const find = digiArr.find(
+    ({ name }) => name.toLowerCase() === target.value.toLowerCase()
+  );
+
+  localStorage.setItem("digimon", JSON.stringify(find));
+
+  if (!find) return alert("Digimon não encontrado");
+
+  const { name, img, level } = find;
+
+  const storedDigimon = JSON.parse(locationStorage.getItem("digimon"));
+
+  if (storedDigimon) {
+    const {
+      name: storedName,
+      img: storedImg,
+      level: storedLevel,
+    } = storedDigimon;
+    imgDigi.src = storedImg;
+    digiTitle.innerText = storedName;
+    digiText.innerText = `${storedName} Digimon do tipo ${storedLevel}`
+  }
+  else {
+    imgDigi.src = img;
+    digiTitle.innerText = name;
+    digiText.innerText = `${name} Digimon do tipo ${level}`
+  }
+
+}
+
 function digiAPI() {
-
-  input.addEventListener('change', async ({ target }) => {
-    const digiArr = await fetchAPI();
-
-    const find = digiArr.find(({ name }) => name.toLowerCase() === target.value.toLowerCase());
-
-    const image = find.img;
-    const { name, level } = find;
-
-    digiText.innerText = `${""} digimon do tipo ${level}`;
-    digiTitle.innerHTML = name;
-    imgDigi.src = image;
-
-    return find;
-  })
+  input.addEventListener('change', digiCard)
+    
 }
 
 window.onload = () => digiAPI();
